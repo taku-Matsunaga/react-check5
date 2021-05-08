306b38aeb2585d26d52ea71ae94bfc0fb15457c7
taku matsunaga <skyline199306@yahoo.co.jp>
2021-05-08 14:22:11 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const onClickUsers = () => {
+    axios.get("https://jsonplaceholder.typicode.com/users").then((res) => {
+    console.log(res.data);
+    }).catch((err) => console.log(err))
+  }
+
+  const onClickUser1 = () => {
+    axios.get("https://jsonplaceholder.typicode.com/users/1").then((res) => {
+      console.log(res.data);
+      }).catch((err) => console.log(err))
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={onClickUsers}>users</button>
+      <button onClick={onClickUser1}>id=1のuser</button>
     </div>
   );
 }
